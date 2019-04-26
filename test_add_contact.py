d6de98883ec2a4cdd04abfb1bb45454d86db22a3
sergepv <serge@ncryptedcloud.com>
2019-04-26 13:41:49 +0300

    Pytest as framework

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,25 +1,21 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 from contact import Contact
 from application import Application
 
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-
-    def test_add_contact(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_new_contact(Contact(firstname='test', lastname='test', middlename='test', nickname='test',
-                                        title='Mr', company='none', address='Addr1', home='123123123',
-                                        mobile='123123123', work='123123123', fax='123123123', email='123@123.123',
-                                        bday='7', bmonth='June', byear='1977', aday='12', amonth='August',
-                                        ayear='2000', address2='none', phone2='none', notes='none'))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.create_new_contact(Contact(firstname='test', lastname='test', middlename='test', nickname='test',
+                                   title='Mr', company='none', address='Addr1', home='123123123',
+                                   mobile='123123123', work='123123123', fax='123123123', email='123@123.123',
+                                   bday='7', bmonth='June', byear='1977', aday='12', amonth='August',
+                                   ayear='2000', address2='none', phone2='none', notes='none'))
+    app.logout()
