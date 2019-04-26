eb34648af46dbe73f07060ccae884241c503f468
sergepv <serge@ncryptedcloud.com>
2019-04-26 15:48:14 +0300

    Common fixture configtest

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,13 +1,3 @@
-import pytest
-
-from fixture.application import Application
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_delete_first_group(app):
