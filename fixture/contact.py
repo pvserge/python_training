c9d197c0375f8bbb459e5f6d714ea9c52a9a9b98
Sergey Perepelov <pvserge1@gmail.com>
2019-05-04 21:34:41 +0300

    Navigation was optimized

--- fixture/contact.py
+++ fixture/contact.py
@@ -8,7 +8,8 @@ class ContactHelper:
 
     def open_contact_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("/index.php") and len(wd.find_elements_by_link_text("Last name")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
