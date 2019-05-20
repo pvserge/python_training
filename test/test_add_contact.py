93ea29c12694ddc5a9a1009ef850b4b5647f2251
sergepv <serge@ncryptedcloud.com>
2019-05-20 12:11:51 +0300

    Command line options

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -27,7 +27,7 @@ def random_email(maxlen1, maxlen2):
 
 testdata = [Contact(firstname="", lastname="", address="", homephone="", mobilephone="", workphone="",
                     secondaryphone="", email1="", email2="", email3="")] + [
-    Contact(firstname="Ivan", lastname="Ivanov", address="SomeWare St., 4", homephone="+111111111",
+    Contact(firstname="Ivan", lastname="Ivanov", address="SomeWhere St., 4", homephone="+111111111",
             mobilephone="(222)222222", workphone="333-333-333", secondaryphone="444444444",
             email1="ii@email1.email", email2="ii@email2.email", email3="ii@email3.email")] + [
     Contact(firstname=random_string("FirstName", 10), lastname=random_string("LastName", 10), address=random_string("Addr1", 20),
