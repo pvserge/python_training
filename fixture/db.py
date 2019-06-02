563e2506e3cb0ab8d3be609528c75c861115277e
Sergey Perepelov <pvserge1@gmail.com>
2019-06-03 00:30:42 +0300

    ORM

--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,6 @@
 import pymysql.cursors
 from model.group import Group
+from model.contact import Contact
 
 
 class DbFixture:
@@ -19,6 +20,22 @@ class DbFixture:
             for row in cursor:
                 (id, name, header, footer) = row
                 list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname, address, home, mobile, work, phone2, email, email2, email3"
+                           " from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname, address, homephone, mobilephone, workphone, secondaryphone,
+                 email, email2, email3) = row
+                list.append(Contact(id=id, firstname=firstname, lastname=lastname, address=address, homephone=homephone,
+                                    workphone=workphone, secondaryphone=secondaryphone, email1=email, email2=email2,
+                                    email3=email3))
         finally:
             cursor.close()
         return list
